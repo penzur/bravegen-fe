8e1b5cc09b0ba4bc959e02b692cd5b74222e2db6
Elizar Pepino <jupenz@gmail.com>
2025-12-09 16:31:12 +0800

    chore: set up font-awesome

---
+++ app/components/icons/fa.tsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { type IconDefinition } from '@fortawesome/fontawesome-svg-core'
+
+type FAIconProps = {
+  icon: IconDefinition
+  className?: string
+}
+
+export function FAIcon({ icon, className }: FAIconProps) {
+  return <FontAwesomeIcon icon={icon} className={className} />
+}
